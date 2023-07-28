f511ece07c2fe635be92c434d378e17709847b8e
walkmoody <moodywalkertx@gmail.com>
2023-07-28 12:13:00 -0500

    Testing with output

--- main.cpp
+++ main.cpp
@@ -1,18 +1,42 @@
 #include "include/raylib.h"
 
+
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    const int screenWidth = 1080; // FIX
+    const int screenHeight = 720;
 
-    while (!WindowShouldClose())
+    InitWindow(screenWidth, screenHeight, "Test");
+
+    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 }; // REMOVE BALL OBJECT
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        
+        
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+            ClearBackground(BLACK);
+            
+
+            DrawText("Congrats! You created your first window!", screenWidth/4, screenHeight/3, 20, LIGHTGRAY);
+
+            DrawCircleV(ballPosition, 50, MAROON);
+
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
