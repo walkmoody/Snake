621384a9ed0b0c478a35369f285d6685c82bceb2
walkmoody <moodywalkertx@gmail.com>
2023-07-28 21:22:20 -0500

    Added snake controls

--- Snake.cpp
+++ Snake.cpp
@@ -3,24 +3,63 @@
 #include "Snake.hpp"
 
 void Snake::initSnake(){
-    snakePosX = screenWidth/2;
-    snakePosY = screenHeight/2;
+    snakePosX = 400;
+    snakePosY = 300;
+    right = false;
+    left = false;
+    down = false;
+    up = false;
     snake = LoadImage("images/Snake.png");   
     ImageResize(&snake, 35, 35) ;
     snakeSkin = LoadTextureFromImage(snake);
     UnloadImage(snake);     
 }
-void Snake::inputSnake(){
-    if( snakePosX % 5 == 0)
-    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) 
-        snakePosX += 5.0f;
-    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) 
-        snakePosX -= 5.0f;
-    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) 
-        snakePosY -= 5.0f;
-    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) 
-        snakePosY += 5.0f;
+
+void Snake::snakeMovement(float x, float y){
+    snakePosX += x;
+    snakePosY += y;
+    if (x > 100)
+        snakePosX -= x;
 }
+void Snake::inputSnake(){
+    //if( snakePosX % 5 == 0)
+    
+if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && left != true){
+        right = true;
+        left = false;
+        down = false;
+        up = false;
+    }
+    else if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) && right != true){
+        right = false;
+        left = true;
+        down = false;
+        up = false;
+    }
+    else if ((IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) && up != true){
+        right = false;
+        left = false;
+        down = true;
+        up = false;
+    }
+    else if ((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) && down != true){
+        right = false;
+        left = false;  
+        down = false;
+        up = true;
+    }
+    
+    if (right == true)
+        snakeMovement(5,0);
+    if (left == true)
+        snakeMovement(-5,0);
+    if (up == true)
+        snakeMovement(0,-5);
+    if (down == true)
+        snakeMovement(0,+5);
+    
+}
+
 void Snake::printSnake(){
     DrawTexture(snakeSkin, snakePosX, snakePosY, WHITE);
     //DrawRectangle(snakePosX, snakePosY, 35, 35, RED);
--- Snake.hpp
+++ Snake.hpp
@@ -6,9 +6,11 @@ class Snake{
         void initSnake();
         void inputSnake();
         void printSnake();
+        void snakeMovement(float, float);
     private:
         const int screenWidth = 1080; // FIX
         const int screenHeight = 720;
+        bool right, left, down, up = false;
         int snakePosX, snakePosY;
         Image snake;
         Texture2D snakeSkin;
